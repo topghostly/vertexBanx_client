6678ad63171f6c4ae5df406075af267e2196d77a
david <td.bolaji@gmail.com>
2024-08-20 11:26:02 +0000

    lasts commit

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -22,9 +22,9 @@ function Admin() {
         `https://vertex-server-9jyo.onrender.com/v0/api/verify/get-transfer/${id}`
       );
 
-      if (!transactionDetails.data.date.beneficiaryLocation.country) {
+      if (transactionDetails.data.date.btransactionStatus === "Pending") {
         setLoading(false);
-        return alert("The transcation has not been completed");
+        return alert("The transaction has not been completed");
       }
 
       setDetails(transactionDetails.data.date);
@@ -48,16 +48,6 @@ function Admin() {
   return (
     <Wrapper>
       <Holder>
-        {/* <Auth>
-          <div className="logo">
-            <img src={logo} alt="admin-logo" />
-          </div>
-          <form>
-            <input type="mail" placeholder="Email Address" />
-            <input type="password" placeholder="Password" />
-            <button>Sign in</button>
-          </form>
-        </Auth> */}
         <MainPage>
           <nav>
             <div className="logo">
@@ -208,61 +198,6 @@ const Holder = styled.div`
   margin: 0px auto;
   min-height: 100vh;
 `;
-
-// const Auth = styled.section`
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-//   display: flex;
-//   flex-direction: column;
-//   gap: 30px;
-//   background-color: var(--light-grey);
-//   padding: 100px 15px;
-//   border-radius: 15px;
-
-//   .logo {
-//     width: 200px;
-//     margin: 0px auto;
-//     pointer-events: none;
-//     img {
-//       width: 100%;
-//       position: relative;
-//     }
-//   }
-
-//   form {
-//     display: flex;
-//     flex-direction: column;
-//     gap: 15px;
-//     width: 99vw;
-//     max-width: 400px;
-//     justify-content: center;
-//     align-items: center;
-
-//     input {
-//       width: 90%;
-//       max-width: 400px;
-//       height: 50px;
-//       outline: none;
-//       /* background-color: var(--light-grey); */
-//       background-color: white;
-//       border: none;
-//       border-radius: 5px;
-//       padding-left: 10px;
-//     }
-
-//     button {
-//       width: 110px;
-//       height: 35px;
-//       border: none;
-//       background-color: black;
-//       color: white;
-//       border-radius: 5px;
-//       cursor: pointer;
-//     }
-//   }
-// `;
 
 const MainPage = styled.section`
   position: relative;
